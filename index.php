11e42e1027c121a5a0ba9c751105cf77a74fc8d0
Sultan <sultan94@gmail.com>
2026-09-08 09:38:24 -0700

    login

---
+++ index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container mt-5 w-25">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header text-bg-success text-center"><h3>LogIn</h3></div>
+                    <div class="card-body">
+                        <form action="" method="POST">
+                            <label for="">Name</label>
+                            <input type="text" class="form-control">
+                            <label for="">Email</label>
+                            <input type="email" class="form-control">
+                            <label for="">Password</label>
+                            <input type="password" class="form-control">
+                            <label for="">Confrim Password</label>
+                            <input type="password" class="form-control">
+                           <div class="text-center mt-3">
+                             <small >SignUp Here <a href="signup.php">SignUp</a></small>
+                           </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+  </body>
+</html>
