e67023226238fe9ee80a2fbc46c58cfe48235878
Sultan <sultan94@gmail.com>
2026-09-08 09:39:34 -0700

    signup

---
+++ add.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Add Student</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container mt-5 w-25">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header text-bg-success text-center"><h3>Add Student</h3></div>
+                    <div class="card-body">
+                        <form action="" method="POST">
+                            <label for="">Name</label>
+                            <input type="text" class="form-control">
+                            <label for="">Email</label>
+                            <input type="email" class="form-control">
+                            <label for="">Class</label>
+                            <input type="text" class="form-control">
+                            <label for="">RollNo</label>
+                            <input type="number" class="form-control">
+                          
+                           <div class="text-center">
+                             <button class="text-center mt-3 btn btn-success">ADD</button>
+                           </div>
+                          
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+  </body>
+</html>
