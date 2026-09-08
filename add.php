176dae68c21013b2fe0fcc0c838fe1d220c6800e
Sultan <sultan94@gmail.com>
2026-09-08 10:28:07 -0700

    newone

--- add.php
+++ add.php
@@ -33,6 +33,9 @@
             </div>
         </div>
     </div>
+    <?php
+echo "new comment";
+    ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
   </body>
 </html>
